506c944ab024be3fef69adbd858cfd52876c7ed0
shashi <shashi2017razz@gmail.com>
2026-05-20 21:56:58 +0530

    closed CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://multi-objective-recommender.vercel.app","http://localhost:5173",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
